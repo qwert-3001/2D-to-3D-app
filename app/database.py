aa1de829a440a2e36944cc6901826881fce5c4a0
Nikita <qwert3001nik@gmail.com>
2026-04-09 16:46:21 +0300

    Add tsr model

--- app/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import decl_base, sessionmaker
-
-engine = create_async_engine("sqlite+aiosqlite:///./test.db")
-async_session = async_sessionmaker(engine, expire_on_commit=False)
-
